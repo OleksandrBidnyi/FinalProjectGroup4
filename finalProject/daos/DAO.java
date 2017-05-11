873ed4b0dcc782521e46a7e7ce8107c87759d0e2
DmytroM <avangardi@ukr.net>
2017-05-12 02:08:17 +0300

    Update DAO.java

--- finalProject/daos/DAO.java
+++ finalProject/daos/DAO.java
@@ -3,9 +3,6 @@ package finalProject.daos;
 import java.util.List;
 
 /**
- * Created by Aleksandr on 26.04.2017.
- */
-/**
  * DAO - interface with methods for accessing the optional work with the Hotel, Room and User .
  */
 public interface DAO<T> {
