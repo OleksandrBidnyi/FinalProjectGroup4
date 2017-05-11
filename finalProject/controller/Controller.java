25d7b9de2c9dde98fadba5bfe0ab60e488a9e0d0
DmytroM <avangardi@ukr.net>
2017-05-12 01:58:07 +0300

    Update Controller.java

--- finalProject/controller/Controller.java
+++ finalProject/controller/Controller.java
@@ -17,6 +17,9 @@ import java.util.stream.Collectors;
 /**
  * Created by Aleksandr on 29.04.2017.
  */
+/**
+ * Class with internal functionality.
+ */
 public class Controller implements ControllerInterface {
     public static final String CITY = "city";
     public static final String HOTEL_NAME = "hotelName";
@@ -24,7 +27,10 @@ public class Controller implements ControllerInterface {
     public static final String MAX_PRICE = "maxPrice";
     public static final String MIN_PRICE = "minPrice";
 
-    //user registration in base
+     /**
+     * registerUser method for registration new user in User's DataBase.
+     * @param newUser - new user for registration.
+     */   
     public boolean registerUser(User newUser) {
         long id;
         if (UserDAO.getUserDAO().getBase().size() != 0) {
@@ -38,7 +44,12 @@ public class Controller implements ControllerInterface {
         return b;
     }
 
-    //Enter to system for user
+     /**
+     * enter method is for entering User to system.
+     * @param name - name for enter to the system.
+     * @param pass - password for enter to the system.
+     * @return - returns user that passed into system(or not passed).
+     */
     public User enter(String name, String pass) {
         if (UserDAO.getUserDAO().getBase().size() == 0) {
             System.out.println("User hasn`t been found");
@@ -57,7 +68,11 @@ public class Controller implements ControllerInterface {
         return user;
     }
 
-    //find reserved rooms by user
+     /**
+     * bookedUser method is to find reserved rooms by user.
+     * @param user - user that reserved room.
+     * @return - list of rooms that were reserved.
+     */   
     public List<Room> bookedByUser(User user) {
         List<Room> list = RoomDAO.getRoomDAO().getBase().stream().filter(room ->
                 room.getUserReserved() != null).filter(room -> room.getUserReserved().
@@ -68,6 +83,11 @@ public class Controller implements ControllerInterface {
         return list;
     }
 
+      /**
+     * findHotelByName method make a search Hotels by their names.
+     * @param name - Hotel's name.
+     * @return - list of Hotels, that was searching.
+     */
     public List<Hotel> findHotelByName(String name) {
         List<Hotel> list = HotelDAO.getHotelDAO().getBase().stream().filter(hotel ->
                 hotel.getName().toLowerCase().contains(name.toLowerCase())).collect(Collectors.toList());
@@ -77,6 +97,11 @@ public class Controller implements ControllerInterface {
         return list;
     }
 
+     /**
+     * findHotelByCity - method make a search Hotels by their cities.
+     * @param city - City's name.
+     * @return - list of Hotels, that was searching.
+     */
     public List<Hotel> findHotelByCity(String city) {
         List<Hotel> list = HotelDAO.getHotelDAO().getBase().stream().filter(hotel ->
                 hotel.getCity().toLowerCase().contains(city.toLowerCase())).collect(Collectors.toList());
@@ -86,6 +111,12 @@ public class Controller implements ControllerInterface {
         return list;
     }
 
+     /**
+     * bookRoom - method makes room reservation.
+     * @param roomId - the id of a room.
+     * @param userId - the id of user for who is registering a room.
+     * @param hotelId - the id of Hotel for the room.
+     */
     public boolean bookRoom(long roomId, long userId, long hotelId) {
         try {
             User userToRegister = UserDAO.getUserDAO().getBase().stream().
@@ -117,6 +148,12 @@ public class Controller implements ControllerInterface {
         return false;
     }
 
+    /**
+     * cancelReserve - method makes a cancel of registration a room for user.
+     * @param roomId - the id of room.
+     * @param userId - the id of user for who was registered a room.
+     * @param hotelId -the id of room's Hotel.
+     */
     public boolean cancelReserve(long roomId, long userId, long hotelId) {
         try {
             User userToRegister = UserDAO.getUserDAO().getBase().stream().
@@ -149,6 +186,11 @@ public class Controller implements ControllerInterface {
         return false;
     }
 
+     /**
+     * findRoom - method is founding a room by certain criteria.
+     * @param params - these are the criteria for looking for a room.
+     * @return - is returning collections of rooms, that were searching.
+     */
     public List<Room> findRoom(Map<String, String> params) {
         List<Room> rooms = RoomDAO.getRoomDAO().getBase().stream().filter(room ->
                 room.getUserReserved() == null).collect(Collectors.toList());
@@ -207,10 +249,20 @@ public class Controller implements ControllerInterface {
                 (room.getPrice() >= minPrice)).collect(Collectors.toList());
     }
 
+     /**
+     * editUser - method to edit a data of user from User's DataBase.
+     * @param user - user from User's DataBase.
+     * @return - user what was edited.
+     */
     public boolean editUser(User user) {
         return UserDAO.getUserDAO().edit(user);
     }
 
+    /**
+     * findUserById - method to find a User by he's id.
+     * @param id - user's id.
+     * @return - user that was searched.
+     */
     public User findUserById(long id) {
         User user = null;
         try {
@@ -221,18 +273,38 @@ public class Controller implements ControllerInterface {
         return user;
     }
 
+      /**
+     * removeUser - method makes remove user from User's DataBase.
+     * @param user - user to remove.
+     * @return - user that was removed.
+     */
     public boolean removeUser(User user) {
         return UserDAO.getUserDAO().remove(user);
     }
 
+      /**
+     * removeHotel = method makes remove hotel from Hotel's DataBase.
+     * @param hotel - hotel to remove.
+     * @return - hotel that was removed.
+     */
     public boolean removeHotel(Hotel hotel) {
         return HotelDAO.getHotelDAO().remove(hotel);
     }
 
+     /**
+     * removeRoom - method makes remove room from Room's DataBase.
+     * @param room - room to remove.
+     * @return - room that was removed.
+     */
     public boolean removeRoom(Room room) {
         return RoomDAO.getRoomDAO().remove(room);
     }
 
+      /**
+     * findRoomById - method to find a Room by it's id.
+     * @param id - room's id.
+     * @return - room that was searched.
+     */
     public Room findRoomById(long id) {
         Room room = null;
         try {
@@ -242,6 +314,11 @@ public class Controller implements ControllerInterface {
         return room;
     }
 
+     /**
+     * editHotel - method to edit data of hotel from Hotel's DataBase.
+     * @param hotel - hotel to edit.
+     * @return - edited hotel.
+     */
     public Hotel editHotel (Hotel hotel){
         if (HotelDAO.getHotelDAO().edit(hotel)){
             return hotel;
@@ -250,11 +327,21 @@ public class Controller implements ControllerInterface {
         }
     }
 
+     /**
+     * editRoom - method to edit data of room from Room's DataBase.
+     * @param room - room to edit.
+     * @return - edited room.
+     */
     public Room editRoom(Room room){
         if (RoomDAO.getRoomDAO().edit(room)) return room;
         return null;
     }
 
+     /**
+     * addHotel - method add hotel to Hotel's DataBase.
+     * @param hotel - hotel to add.
+     * @return - hotel that was added.
+     */
     public Hotel addHotel (Hotel hotel){
         long id;
         if (HotelDAO.getHotelDAO().getBase().size() != 0){
@@ -270,6 +357,11 @@ public class Controller implements ControllerInterface {
         return null;
     }
 
+      /**
+     * addRoom - method add room to Room's DataBase.
+     * @param room - room to add.
+     * @return - room that was added.
+     */
     public Room addRoom(Room room){
         long id;
         if(RoomDAO.getRoomDAO().getBase().size() != 0){
@@ -283,6 +375,11 @@ public class Controller implements ControllerInterface {
         return null;
     }
 
+      /**
+     * findHotelById - method to find hotel by it's id.
+     * @param id - hotel's id.
+     * @return - hotel that was searched.
+     */
     public Hotel findHotelById(long id){
         Hotel hotel = null;
         try{
@@ -294,6 +391,11 @@ public class Controller implements ControllerInterface {
         return  hotel;
     }
 
+      /**
+     * findUsersByName - method to find users from User's DataBase by their names.
+     * @param name - user's searching name.
+     * @return - list of users, that were searching for.
+     */
     public List<User> findUsersByName (String name){
         List <User> list = UserDAO.getUserDAO().getBase().stream().filter(user ->
         user.getName().toLowerCase().contains(name.toLowerCase())).collect(Collectors.toList());
@@ -302,6 +404,10 @@ public class Controller implements ControllerInterface {
         return list;
     }
 
+    /**
+     * booked - method that shows booked rooms from Room's DataBase.
+     * @return - collections of rooms.
+     */
     public List<Room> booked(){
         return RoomDAO.getRoomDAO().getBase().stream().filter(user->
         user.getUserReserved()!=null).collect(Collectors.toList());
