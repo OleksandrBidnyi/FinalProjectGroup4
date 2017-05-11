1524a6a5480c610476136095f7c70d54dc8c4f14
DmytroM <avangardi@ukr.net>
2017-05-12 02:08:54 +0300

    Update UserDAO.java

--- finalProject/daos/UserDAO.java
+++ finalProject/daos/UserDAO.java
@@ -11,9 +11,6 @@ import java.util.NoSuchElementException;
 import java.util.stream.Stream;
 
 /**
- * Created by Aleksandr on 26.04.2017.
- */
-/**
  * UserDAO is a realization of interface DAO with his own fields and methods that can add,
  * remove and edit User in User's DataBase.
  */
