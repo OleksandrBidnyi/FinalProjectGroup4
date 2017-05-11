9748ab8b32b2710a983fb4bf0c88ea4a9d9a711b
DmytroM <avangardi@ukr.net>
2017-05-11 22:07:39 +0300

    Update Start.java

--- finalProject/startApp/Start.java
+++ finalProject/startApp/Start.java
@@ -8,7 +8,8 @@ import java.io.IOException;
  * Created by Aleksandr on 10.05.2017.
  */
 /**
- * This section provides log on for the administrator and the user
+ * This section provides log on for the administrator and the user.
+ * To log in as administrator you must to use following fields: admin/0000.
  */
 public class Start {
     public static void main(String[] args) throws IOException {
