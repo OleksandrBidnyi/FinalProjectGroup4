5e2c27daff33b5eb98bb9450ab868c7be0137d9a
DmytroM <avangardi@ukr.net>
2017-05-11 22:01:03 +0300

    Update Start.java

--- finalProject/startApp/Start.java
+++ finalProject/startApp/Start.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 /**
  * Created by Aleksandr on 10.05.2017.
  */
+/**
+ * This section provides log on for the administrator and the user
+ */
 public class Start {
     public static void main(String[] args) throws IOException {
         UserVsAdmin userVsAdmin = new UserVsAdmin();
